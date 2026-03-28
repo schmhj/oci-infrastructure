5d7c1e180c84560e160a084266410e02d99a29a6
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2026-03-28 17:40:37 -0400

    oci-infra and k8s infra

---
+++ oci-infra/variables.tf
@@ -0,0 +1,20 @@
+variable "compartment_id" {
+  type        = string
+  description = "The compartment to create the resources in"
+}
+
+variable "region" {
+  type        = string
+  description = "The region to provision the resources in"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "The SSH public key to use for connecting to the worker nodes"
+}
+
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version"
+  default     = "v1.31.10"
+}
