400964796c826d14e6ae2c53728299244c63cc96
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2026-04-08 14:28:04 -0400

    chore: update cicd pipeline

--- scripts/setup_sa.sh
+++ scripts/setup_sa.sh
@@ -40,4 +40,5 @@ EOF
 fi
 
 echo "Retrieving token..."
-echo "$(kubectl get secret tfc-deployer-token -n kube-system -o jsonpath='{.data.token}' | base64 --decode)"
+
+export K8S_TOKEN=$(kubectl get secret tfc-deployer-token -n kube-system -o jsonpath='{.data.token}' | base64 --decode)"
